c2652e866c1a69f71b0a81b72d1b1efd9686f17b
Chris <chrishughes.bristol@gmail.com>
2024-01-24 12:40:51 +0000

    Added my thoughts to the top of the file.

--- OverEngineeredHelloWorld/src/test/java/MemorySinkTest.java
+++ OverEngineeredHelloWorld/src/test/java/MemorySinkTest.java
@@ -1,3 +1,13 @@
+
+// CH: The following is a collection of my thoughts and findings after generating tests using JetBrains AI.
+// There are options in the context menu to generate tests, but I couldn't get these to work at either class or method
+// level, or by selecting the code I wanted to test. I could however open the swirly AI tab on the side menu and asked
+// it to generate unit tests that I could copy and paste into this test class. These are the results.
+// The generated code required some editing of course to be able to build, but these were similar with other plugins I
+// have tried. Overall the three tests generated are valuable and good quality, although some more strictly minded
+// people might point out that each test has multiple assertions. Personally I think of myself as being a bit more
+// pragmatic and because each test is against the same simple object I think it's not worth taking issue with.
+
 import org.exploration.sinks.MemorySink; // CH: Added to resolve error with the memorySink field.
 import org.junit.jupiter.api.BeforeEach; // CH: Updated to correct path.
 import org.junit.jupiter.api.Test; // CH: Updated to correct path.
